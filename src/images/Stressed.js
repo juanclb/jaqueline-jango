d1590e415949d07f232e1099ece0ac5ca1d80684
Juan <juan@News-MacBook-Pro.local>
2025-03-21 21:29:54 -0300

    [FIX] Removed ununsed imports in SVGs

--- src/images/Stressed.js
+++ src/images/Stressed.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const Stressed = (props) => (
+const Stressed = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="100%"
--- src/images/WomanThinking.js
+++ src/images/WomanThinking.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 
-const WomanThinking = (props) => (
+const WomanThinking = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={"100%"}
